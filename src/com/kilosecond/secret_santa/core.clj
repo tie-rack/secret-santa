82b840edf17781b8b00c9791576488d3eccd89ca
Chris Shea <cmshea@gmail.com>
2012-12-12 14:00:24 -0500

    Work directly with lists of pairs, not two lists.
    no-doubleso makes more sense than position-distincto, since it
    works directly with the thing we're interested in, and not some
    intermediate form.
    
    Also, some renamings.

--- src/com/kilosecond/secret_santa/core.clj
+++ src/com/kilosecond/secret_santa/core.clj
@@ -2,51 +2,46 @@
   (:refer-clojure :exclude [==])
   (:use [clojure.core.logic]))
 
-(defne position-distincto
-  "A relation where xl and yl have different members at each position"
-  [xl yl]
-  ([() ()])
-  ([[x . xs] [y . ys]]
-     (position-distincto xs ys)
-     (!= x y)))
-
-(defn strict-permuteo
-  "A relation where xl and yl are permutations of each other and have
-  distinct members at each position"
-  [xl yl]
-  (all
-   (permuteo xl yl)
-   (position-distincto xl yl)))
+(defne no-doubleso
+  "pairs does not have a pair with identical members"
+  [pairs]
+  ([()])
+  ([[p . ps]]
+     (fresh [a b]
+            (== p [a b])
+            (!= a b)
+            (no-doubleso ps))))
 
 (defne pairso
-  "xl is zipped pairs of yl and zl"
-  [xl yl zl]
+  "pairs is zipped pairs of xl and yl"
+  [pairs xl yl]
   ([() () ()])
-  ([[x . xs] [y . ys] [z . zs]]
-     (== x [y z])
-     (pairso xs ys zs)))
+  ([[p . ps] [x . xs] [y . ys]]
+     (== p [x y])
+     (pairso ps xs ys)))
 
-(defne not-pairso
-  "x and y aren't paired in pl"
-  [pl x y]
+(defne not-pairedo
+  "x and y aren't paired in pairs"
+  [pairs x y]
   ([() _ _])
   ([[p . ps] _ _]
      (!= p [x y])
      (!= p [y x])
-     (not-pairso ps x y)))
+     (not-pairedo ps x y)))
 
 (defn santa-pairso
-  "Pairs"
+  "Can generate Secret Santa pairings from a list of gifters"
   [pairs gifters]
-  (fresh [giftees]
-         (strict-permuteo gifters giftees)
-         (pairso pairs gifters giftees)))
+  (fresh [recipients]
+         (permuteo gifters recipients)
+         (pairso pairs gifters recipients)
+         (no-doubleso pairs)))
 
 (defmacro secret-santa
   "Generate a secret santa pairing from names, excluding pairs in
   bad-pairs."
   [names bad-pairs]
-  (let [exclusions (map (fn [[p1 p2]] `(not-pairso ~'q ~p1 ~p2)) bad-pairs)]
+  (let [exclusions (map (fn [[p1 p2]] `(not-pairedo ~'q ~p1 ~p2)) bad-pairs)]
     `(first (run 1 [~'q]
                  (santa-pairso ~'q ~names)
                  ~@exclusions))))
